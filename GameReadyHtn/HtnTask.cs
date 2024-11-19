456e786f19e6d02064116f571eeef256418e612e
Joyless <65855333+Joy-less@users.noreply.github.com>
2024-11-19 02:41:04 +0000

    Add doc comments to HtnTasks

--- GameReadyHtn/HtnTask.cs
+++ GameReadyHtn/HtnTask.cs
@@ -1,5 +1,8 @@
 ﻿namespace GameReadyHtn;
 
+/// <summary>
+/// A task a <see cref="HtnAgent"/> can perform, changing its states.
+/// </summary>
 public abstract class HtnTask(object? Name = null) {
     /// <summary>
     /// An optional identifier.
@@ -14,33 +17,15 @@ public abstract class HtnTask(object? Name = null) {
     /// </summary>
     /// <remarks>By default, always returns null.</remarks>
     public Func<HtnAgent, bool?> IsValidOverride = _ => null;
-
-    /*/// <summary>
-    /// Gets all of the effects of the task including compound tasks.
-    /// </summary>
-    public abstract IEnumerable<HtnEffect> GetEffects();
-    /// <summary>
-    /// Gets the predicted states after the action is performed.
-    /// </summary>
-    public Dictionary<object, object?> PredictStates(IReadOnlyDictionary<object, object?> States) {
-        Dictionary<object, object?> PredictedStates = new(States);
-        foreach (HtnEffect Effect in Effects) {
-            PredictedStates[Effect.State] = Effect.PredictState(PredictedStates);
-        }
-        return PredictedStates;
-    }*/
 }
+/// <summary>
+/// A task that runs a raw action with a list of effects.
+/// </summary>
 public class HtnPrimitiveTask(object? Name = null) : HtnTask(Name) {
     /// <summary>
     /// The effects that the task is predicted to have on the agent's states.
     /// </summary>
     public required List<HtnEffect> Effects;
-
-    /*public override IEnumerable<HtnEffect> GetEffects() {
-        foreach (HtnEffect Effect in Effects) {
-            yield return Effect;
-        }
-    }*/
 
     /// <summary>
     /// Gets the predicted states after the action is performed.
@@ -53,18 +38,18 @@ public class HtnPrimitiveTask(object? Name = null) : HtnTask(Name) {
         return PredictedStates;
     }
 }
+/// <summary>
+/// A task that is completed by choosing the first valid task.
+/// </summary>
 public class HtnSelectorTask(object? Name = null) : HtnTask(Name) {
     /// <summary>
     /// The choices of task that could be run to complete this task.
     /// </summary>
     public List<HtnTask> Tasks = [];
-
-    /*public override IEnumerable<HtnEffect> GetEffects() {
-        foreach (HtnTask Task in Tasks) {
-            yield return Effect;
-        }
-    }*/
 }
+/// <summary>
+/// A task that is completed by running each task in order.
+/// </summary>
 public class HtnSequenceTask(object? Name = null) : HtnTask(Name) {
     /// <summary>
     /// The tasks that should be run to complete this task.
